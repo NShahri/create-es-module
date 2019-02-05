9e63b3a9a7b2de45a1d305889a556b6961bd8dfa
Nima <nima@movio.co>
2019-02-05 17:21:46 +1300

    fix babel plugin for esm

--- src/templates/babel.config.js
+++ src/templates/babel.config.js
@@ -3,6 +3,16 @@
 const presets = [['@babel/preset-env', {loose: true}], '@babel/preset-react', '@babel/preset-flow'];
 const plugins = [['@babel/plugin-proposal-class-properties', {loose: true}]];
 
+if (process.env['BABEL_ENV'] === 'esm') {
+    presets[0] = [
+        '@babel/preset-env',
+        {
+            loose: true,
+            modules: false,
+        },
+    ];
+}
+
 if (process.env['BABEL_ENV'] === 'esm' || process.env['BABEL_ENV'] === 'cjs') {
     plugins.push('@babel/plugin-transform-runtime');
 }
